c9407cd4fcf362988ade74b2fd0a4c8fae8825a3
Abdellatif <abdelatifgourri11@gmail.com>
2024-10-20 16:04:40 +0100

    le debut de homePage

---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+const HomePage =({ onStart }){
+             return(
+                     <div className="home-container">
+                        {src/components/background_acceuil.png}    
+                            <div className="background-image">
+                                   <button className="start-button" onClick={onStart}>Voir les repas disponibles</button>
+                            </div>
+                     </div>
+                     
+                    
+                     
+                   )
+}
+
